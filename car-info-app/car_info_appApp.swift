949d0fdb62e8fb3b9f2ac2c5ec2e070181310582
Jihye Park <deviloper.park@gmail.com>
2024-04-16 13:20:42 +0900

    Initial Commit

---
+++ car-info-app/car_info_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  car_info_appApp.swift
+//  car-info-app
+//
+//  Created by 박지혜 on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct car_info_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
