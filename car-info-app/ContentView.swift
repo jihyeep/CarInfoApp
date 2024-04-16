325638f80f31727ed38ac311366a9fbbfece5936
Jihye Park <deviloper.park@gmail.com>
2024-04-16 15:15:50 +0900

    Class instance

--- car-info-app/ContentView.swift
+++ car-info-app/ContentView.swift
@@ -1,1 +1,38 @@
+import SwiftUI
 
+class TopGear: Identifiable {
+    let id = UUID()
+    var name: String
+    var classification: [TopGear]?
+    
+    init(name: String, classification: [TopGear]? = nil) {
+        self.name = name
+        self.classification = classification
+    }
+}
+
+struct ContentView: View {
+    var gear: [TopGear] = [
+        TopGear(name: "전기", classification: [
+            TopGear(name: "x"),
+            TopGear(name: "y"),
+            ]),
+        TopGear(name: "내연기관", classification: [
+            TopGear(name: "k9"),
+            TopGear(name: "k8"),
+            TopGear(name: "k7")
+            ])
+    ]
+    var body: some View {
+        List {
+            OutlineGroup(gear, id: \.id, children: \.classification) {
+                creature in Text(creature.name)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
